d6b3c7906d9230189fae4deb5d58aafcbe3c0fb1
don734 <vip.don4ik@bk.ru>
2023-10-25 22:34:15 +0500

    upd review and news section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     mainSlider();
+    reviewSlider();
+    newsSlider();
 })
 
 function mainSlider() {
@@ -10,6 +12,54 @@ function mainSlider() {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
+    };
+    const swiper = new Swiper(swiperContainer, swiperOptions);
+}
+
+function reviewSlider() {
+    let swiperContainer = '.swiper-review';
+    let swiperOptions = {
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+        },
+        slidesPerView: 1,
+        spaceBetween: 20,
+        clickable: true,
+        breakpoints: {
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            }
+        }
+    };
+    const swiper = new Swiper(swiperContainer, swiperOptions);
+}
+
+function newsSlider() {
+    let swiperContainer = '.swiper-news';
+    let swiperOptions = {
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+        },
+        navigation: {
+            nextEl: '.news .swiper-button-next',
+            prevEl: '.news .swiper-button-prev',
+        },
+        slidesPerView: 1,
+        spaceBetween: 20,
+        clickable: true,
+        breakpoints: {
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            992: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+            },
+        }
     };
     const swiper = new Swiper(swiperContainer, swiperOptions);
 }
