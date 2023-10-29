1a1e472e098e1e9d3e516a47441ccf7030d50e4a
don734 <vip.don4ik@bk.ru>
2023-10-29 21:59:18 +0500

    upd main page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     mainSlider();
+    projectSlider();
+    teamSlider();
     reviewSlider();
     newsSlider();
     partnersSlider();
@@ -11,9 +13,40 @@ function mainSlider() {
     let swiperOptions = {
         loop: true,
         navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
+            nextEl: '.slider .swiper-button-next',
+            prevEl: '.slider .swiper-button-prev',
         },
+    };
+    const swiper = new Swiper(swiperContainer, swiperOptions);
+}
+
+function projectSlider() {
+    let swiperContainer = '.swiper-projects';
+    let swiperOptions = {
+        pagination: {
+            el: '.swiper-pagination',
+        },
+        slidesPerView: 1,
+        spaceBetween: 20,
+        clickable: true,
+        renderBullet: function (index, className) {
+            console.log(index);
+            return '<span class="' + className + '">' + (index + 1) + "</span>";
+        },
+    };
+    const swiper = new Swiper(swiperContainer, swiperOptions);
+}
+
+function teamSlider() {
+    let swiperContainer = '.team .swiper-team';
+    let swiperOptions = {
+        pagination: {
+            el: '.swiper-pagination',
+        },
+        slidesPerView: 'auto',
+        spaceBetween: 55,
+        freeMode: true,
+        clickable: true,
     };
     const swiper = new Swiper(swiperContainer, swiperOptions);
 }
@@ -21,7 +54,6 @@ function mainSlider() {
 function reviewSlider() {
     let swiperContainer = '.swiper-review';
     let swiperOptions = {
-        loop: true,
         pagination: {
             el: '.swiper-pagination',
         },
@@ -39,9 +71,8 @@ function reviewSlider() {
 }
 
 function newsSlider() {
-    let swiperContainer = '.swiper-news';
+    let swiperContainer = '.news .swiper-news';
     let swiperOptions = {
-        loop: true,
         pagination: {
             el: '.swiper-pagination',
         },
@@ -67,11 +98,11 @@ function newsSlider() {
 }
 
 function partnersSlider() {
-    let swiperContainer = '.swiper-partner';
+    let swiperContainer = '.partners .swiper-partner';
     let swiperOptions = {
         navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
+            nextEl: '.partners .swiper-button-next',
+            prevEl: '.partners .swiper-button-prev',
         },
         loop: true,
         slidesPerView: 2,
@@ -93,11 +124,16 @@ function bigAndThumbSlider() {
     let swiperBigContainer = '.swiper-big';
     let swiperThumbContainer = '.swiper-thumbs';
     let swiperThumb = new Swiper(swiperThumbContainer, {
-        direction: 'vertical',
         spaceBetween: 15,
-        slidesPerView: 4,
+        slidesPerView: 2,
         freeMode: true,
         watchSlidesProgress: true,
+        breakpoints: {
+            768: {
+                direction: 'vertical',
+                slidesPerView: 4,
+            },
+        }
     });
     let swiperBig = new Swiper(swiperBigContainer, {
         spaceBetween: 10,
